a6ab62f40f50b7cba817720c2454969a89aa04a4
Ahmed Abdulbaset Abdulhadi Elgammudi <ahmed.elgammudi@neareasttech.local>
2020-06-28 11:31:14 +0300

    Login and Signup and reset password

---
+++ mysite/urls.py
@@ -0,0 +1,26 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from mysite.core import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('secret/', views.secret_page, name='secret'),
+    path('secret2/', views.SecretPage.as_view(), name='secret2'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+]
